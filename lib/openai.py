d1f8435ed55d92c54c6fb11e88a0e314f3709833
rstevens <rstevens@opticomm.net.au>
2023-01-02 17:06:15 +1100

    fix returning of openai error messages

--- lib/openai.py
+++ lib/openai.py
@@ -13,39 +13,38 @@ def imagesGenerations(prompt, size='256x256', number=1):
     except requests.exceptions.ConnectionError as e:
         print("OpenAI:", e, file=stderr)
         return False, e
-    success, message = processOpenaiUrl(r.json())
+    success, message = processOpenaiUrl(r.json(), r.status_code)
     return success, message
 
 def imagesVariations(imagebytes, size='256x256', number=4):
     url = 'https://api.openai.com/v1/images/variations'
     headers = {'Authorization': 'Bearer ' + config_openai_api_key }
     files = {"image": imagebytes}
-    #files = { "image": ("image_1671453832.png", open("image_1671453832.png", "rb")) }
     data = {'size': size, 'n': number, 'response_format': 'url'}
     try:
         r = requests.post(url, headers=headers, data=data, files=files, timeout=30)
     except requests.exceptions.ConnectionError as e:
         print("OpenAI:", e, file=stderr)
         return False, e
-    success, message = processOpenaiUrl(r.json())
+    success, message = processOpenaiUrl(r.json(), r.status_code)
     return success, message
 
-def processOpenaiUrl(result):
+def processOpenaiUrl(result, status_code):
     if 'data' in result:
         if len(result['data']) == 1:
             image_url = result['data'][0]['url']
-        elif len(result['data']) > 1:
+        else:
             image_url = []
             for item in result['data']:
                 #image_url.append(unquote(item['url']))
                 image_url.append(item['url'])
+        return True, image_url
     elif 'error' in result:
         error_code = result['error']['code']
         error_message = result['error']['message']
-        print(r.status_code, error_code, error_message, file=stderr)
+        print(status_code, error_code, error_message, file=stderr)
         return False, error_message
     else:
-        print(r.status_code, 'unknown error communicating with OpenAI')
-        return False, f'{r.status_code} unknown error communicating with OpenAI'
-    return True, image_url
+        print(status_code, 'unknown error communicating with OpenAI')
+        return False, f'{status_code} unknown error communicating with OpenAI'
 
